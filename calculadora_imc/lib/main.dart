0781b68c859657f1528cf2f3b039eda36e437123
Murilo Krugner <murilokrugner@hotmail.com>
2019-01-17 19:59:25 -0200

    alteção no arquivo main.dart

--- calculadora_imc/lib/main.dart
+++ calculadora_imc/lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    title: 'Calculadora para alunos',
     home: Home(),
   ));
 }
@@ -13,52 +12,53 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController weightController = TextEditingController();
+  TextEditingController heightController = TextEditingController();
 
-  TextEditingController _nota1Controller = TextEditingController();
-  TextEditingController _nota2Controller = TextEditingController();
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
-  GlobalKey<FormState> _keyy = GlobalKey<FormState>();
+  String _infoText = "Informe seus dados!";
 
-  String _info = 'A sua média aparece aqui!';
-
-  void _reset(){
+  void _resetFields() {
+    weightController.text = "";
+    heightController.text = "";
     setState(() {
-      _nota1Controller.text = "";
-      _nota2Controller.text = "";
-      _info = 'A sua média aparece aqui!';
-
+      _infoText = "Informe seus dados!";
     });
   }
 
-  void _calculate(){
+  void _calculate() {
     setState(() {
-      double nota1 = double.parse(_nota1Controller.text);
-      double nota2 = double.parse(_nota2Controller.text);
-      double media = (nota1 + nota2) / 2;
-      if(media >= 7.0){
-        _info = 'Você está aprovado!';
-      }
-      else if(media < 7.0 && media >= 6.0){
-        _info = 'Você está de recuperação';
-      }
-      else if(media < 6.0){
-        _info = "Você está reprovado!";
+      double weight = double.parse(weightController.text);
+      double height = double.parse(heightController.text) / 100;
+      double imc = weight / (height * height);
+      if (imc < 18.6) {
+        _infoText = "Abaixo do Peso (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 18.6 && imc < 24.9) {
+        _infoText = "Peso Ideal (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 24.9 && imc < 29.9) {
+        _infoText = "Levemente Acima do Peso (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 29.9 && imc < 34.9) {
+        _infoText = "Obesidade Grau I (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 34.9 && imc < 39.9) {
+        _infoText = "Obesidade Grau II (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 40) {
+        _infoText = "Obesidade Grau III (${imc.toStringAsPrecision(4)})";
       }
     });
-
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Calculadora para alunos'),
+        title: Text("Calculadora de IMC"),
         centerTitle: true,
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.green,
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.refresh),
-            onPressed: _reset,
+            onPressed: _resetFields,
           )
         ],
       ),
@@ -66,66 +66,65 @@ class _HomeState extends State<Home> {
       body: SingleChildScrollView(
         padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
         child: Form(
-            key: _keyy,
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  Icon(
-                    Icons.scanner,
-                    size: 120.0,
-                    color: Colors.blue,
-                  ),
-                  TextFormField(
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(
-                      labelText: 'Nota 1',
-                      labelStyle: TextStyle(color: Colors.blue, fontSize: 25.0),
-                    ),
-                    textAlign: TextAlign.center,
-                    controller: _nota1Controller,
-                    validator: (value){
-                      if(value.isEmpty){
-                        return 'Insira a nota 1';
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Icon(Icons.person_outline, size: 120.0, color: Colors.green),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Peso (kg)",
+                    labelStyle: TextStyle(color: Colors.green)),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.green, fontSize: 25.0),
+                controller: weightController,
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return "Insira seu Peso!";
+                  }
+                },
+              ),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Altura (cm)",
+                    labelStyle: TextStyle(color: Colors.green)),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.green, fontSize: 25.0),
+                controller: heightController,
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return "Insira sua Altura!";
+                  }
+                },
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                child: Container(
+                  height: 50.0,
+                  child: RaisedButton(
+                    onPressed: () {
+                      if (_formKey.currentState.validate()) {
+                        _calculate();
                       }
                     },
-                  ),
-                  TextFormField(
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(
-                      labelText: 'Nota 2',
-                      labelStyle: TextStyle(color: Colors.blue, fontSize: 25.0),
+                    child: Text(
+                      "Calcular",
+                      style: TextStyle(color: Colors.white, fontSize: 25.0),
                     ),
-                    textAlign: TextAlign.center,
-                    controller: _nota2Controller,
-                    validator: (value){
-                      if(value.isEmpty){
-                        return 'Insira a nota 2!';
-                      }
-                    },
+                    color: Colors.green,
                   ),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 10.0),
-                    child: Container(
-                      height: 50.0,
-                      child: RaisedButton(
-                        onPressed: () {
-                          if(_keyy.currentState.validate()){
-                            _calculate();
-                          }
-                        },
-                        child: Text('Verificar',
-                          style: TextStyle(color: Colors.white, fontSize: 25.0),),
-                        color: Colors.blue,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(0.0, 30.0, 0.0, 30.0),
-                    child:Text(_info,
-                      style: TextStyle(color: Colors.blue, fontSize: 25.0),
-                      textAlign: TextAlign.center,),
-                  ),
-                ])),
+                ),
+              ),
+              Text(
+                _infoText,
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.green, fontSize: 25.0),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
